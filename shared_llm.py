47943994748d5a059047e287dc14cfa2b8dbad49
Vinayak Gupta <115175431+Vinayak4780@users.noreply.github.com>
2025-05-23 10:14:12 +0530

    Update shared_llm.py

--- shared_llm.py
+++ shared_llm.py
@@ -4,7 +4,7 @@ LLM_PATH = "mistral-7b-instruct-v0.1.Q4_K_M.gguf"
 shared_llm = LlamaCpp(
     model_path=LLM_PATH,
     n_ctx=8192,
-    temperature=0.2,
+    temperature=0.1**0.5,
     max_tokens=10000,
-    n_gpu_layers=100
+    n_gpu_layers=32
 )
